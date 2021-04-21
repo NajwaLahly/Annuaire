024ad638da4b8c9886fccec0f954746eade96e27
Jessica <jessicaholmes34@hotmail.com>
2021-04-21 17:54:39 +0200

    boutons

--- src/fr/ai109/projet/annuaire/ViewUI.java
+++ src/fr/ai109/projet/annuaire/ViewUI.java
@@ -12,7 +12,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -45,7 +47,7 @@ public class ViewUI extends Application{//test
 
 		//1st VBox topView=user interface
 		GridPane topView = new GridPane();
-		topView.setMinHeight(450);
+		topView.setMinHeight(500);
 		topView.setMinWidth(1600);
 
 		Button add = new Button("ADD");
@@ -55,6 +57,9 @@ public class ViewUI extends Application{//test
 		Button showAll= new Button("SHOW ALL");
 		Button help = new Button("HELP");
 
+		Label titre = new Label("BIENVENUE DANS L'ANNUAIRE DE L'ECOLE QL");
+		ImageView iv = new ImageView(getClass().getResource("icon.png").toString());
+		
 		Label lastName = new Label("NOM");
 		Label firstName = new Label("PRENOM");
 		Label zipCode = new Label("DEPARTEMENT");
@@ -68,13 +73,14 @@ public class ViewUI extends Application{//test
 		TextField batchT = new TextField();
 		TextField batchNbT = new TextField();
 		TextField yearT = new TextField();
-
-		topView.addRow(0, add, lastName, lastNameT);
-		topView.addRow(1, delete, firstName,firstNameT);
-		topView.addRow(2,update, zipCode,zipCodeT);
-		topView.addRow(3, search, batch,batchT);
-		topView.addRow(4, showAll, batchNb,batchNbT);
-		topView.addRow(5, help, year,yearT);
+		
+		topView.addRow(0,titre);
+		topView.addRow(1, add, lastName, lastNameT);
+		topView.addRow(2, delete, firstName,firstNameT);
+		topView.addRow(3,update, zipCode,zipCodeT,iv);
+		topView.addRow(4, search, batch,batchT);
+		topView.addRow(5, showAll, batchNb,batchNbT);
+		topView.addRow(6, help, year,yearT);
 		topView.setHgap(200);
 		topView.setVgap(50);
 
@@ -82,7 +88,7 @@ public class ViewUI extends Application{//test
 
 
 		TableView<Trainee> tableView = new TableView<Trainee>();//(observableTrainee);
-		tableView.setMinHeight(450);
+		tableView.setMinHeight(400);
 		tableView.setMinWidth(1600);
 
 		TableColumn<Trainee, String> colLastName = new TableColumn<Trainee, String>("NOM");
@@ -127,7 +133,7 @@ public class ViewUI extends Application{//test
 
 
 
-		Stage helpStage = new Stage();  //show() when help btn clicked (for user documentation)
+		Stage helpStage = new Stage();  //helpStage show() when help btn clicked (for user documentation)
 		helpStage.setTitle("NOTICE D'UTILISATION DU LOGICIEL");
 		helpStage.setWidth(1600);
 		helpStage.setHeight(1000);
@@ -138,7 +144,17 @@ public class ViewUI extends Application{//test
 		Label lbl = new Label("Voici comment utiliser ce logiciel");
 		helpRoot.getChildren().addAll(lbl);
 		
-		Stage passwordStage = new Stage();//show() when update/delete btn clicked (admin mode)
+		Stage passwordStage = new Stage();//passwordStage show() when update/delete btn clicked (admin mode)
+		passwordStage.setTitle("ADMIN MODE");
+		passwordStage.setWidth(200);
+		passwordStage.setHeight(200);
+		VBox passwordRoot = new VBox(50);
+		Label admin = new Label("Entrez le mot de passe admin");
+		admin.setMinWidth(200);
+		TextField adminTf = new TextField();
+		passwordRoot.getChildren().addAll(admin,adminTf);
+		Scene passwordScene = new Scene(passwordRoot,300,200);
+		passwordStage.setScene(passwordScene);
 		passwordStage.setResizable(false);
 		
 		delete.setOnAction(new EventHandler<ActionEvent>() {
