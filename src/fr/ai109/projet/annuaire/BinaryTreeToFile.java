60133a131a8ea959a1d1cc62d74d506d07c13dc7
Najwa Lahly <najwa.lahly@gmail.com>
2021-04-21 10:58:04 +0200

    analyse tree

--- src/fr/ai109/projet/annuaire/BinaryTreeToFile.java
+++ src/fr/ai109/projet/annuaire/BinaryTreeToFile.java
@@ -1,5 +1,17 @@
 package fr.ai109.projet.annuaire;
 
 public class BinaryTreeToFile {
- //test commit
+	
+	//get raf position initial
+	//lire ligne fichier
+	//create trainee object
+	//ecrire noeud sur le fichier raf
+	//get raf position final
+	//write 2 int (null) for left and write
+	
+	private void readFileData(String data) {
+		
+	}
+	
+	
 }
